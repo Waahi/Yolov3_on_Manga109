4f4916a50bdc48e01c181c3c0ed2781e2efbe5f0
research proposal <37407503+Waahi@users.noreply.github.com>
2020-07-22 17:26:15 +0900

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -24,7 +24,7 @@ class YOLO(object):
         "anchors_path": 'model_data/yolo_anchors.txt',
         "classes_path": 'model_data/coco_classes.txt',
         "score" : 0.3,
-        "iou" : 0.45,
+        "iou" : 0.46,
         "model_image_size" : (416, 416),
         "gpu_num" : 1,
     }
